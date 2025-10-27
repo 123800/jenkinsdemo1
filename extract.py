1164ee447c758bfccbc869b7725f743f8fc032e3
123800 <60835373+123800@users.noreply.github.com>
2025-10-27 19:21:50 +0530

    okk file testing3

--- extract.py
+++ extract.py
@@ -1,5 +1,20 @@
 import pandas as pd
-print("Extract script running successfully!")
+
+print("Extract Data")
+
+# Sample data
+data = {
+    'Id': [101, 102, 103],
+    'Name': ['Ram', 'Raj', 'Raja'],
+    'Age': [29, 34, 42]
+}
+
+# Create DataFrame
+df = pd.DataFrame(data)
+
+# Display the DataFrame
+print(df)
+
 
 
 
